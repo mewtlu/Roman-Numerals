d5c86061c02da4e5fb3ae40e56bf1a5c8599ba33
Luke Roberts <luke@mewt.eu>
2019-07-16 13:34:03 +0100

    Initialize constant array of all Roman numeral digits

--- src/Helper/RomanNumerals.php
+++ src/Helper/RomanNumerals.php
@@ -5,6 +5,16 @@ use \App\Interfaces\RomanNumeralGenerator;
 
 class RomanNumerals implements RomanNumeralGenerator
 {
+    const arrDigits = [
+        1 => 'I',
+        5 => 'V',
+        10 => 'X',
+        50 => 'L',
+        100 => 'C',
+        500 => 'D',
+        1000 => 'M',
+    ];
+
     public static function generate(int $number): String
     {
         return '';
