0258706ed6fcbaa00e335ab2cdce6108a5ba8586
Luke Roberts <luke@mewt.eu>
2019-07-16 13:31:11 +0100

    Create base Roman numerals class implementing interface

---
+++ src/Helper/RomanNumerals.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Helper;
+
+use \App\Interfaces\RomanNumeralGenerator;
+
+class RomanNumerals implements RomanNumeralGenerator
+{
+    public static function generate(int $number): String
+    {
+        return '';
+    }
+}
